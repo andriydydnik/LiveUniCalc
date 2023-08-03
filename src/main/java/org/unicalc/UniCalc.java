15e319aefe720cbffbfcd9a6771c868d54740626
andriydydnik <andriy.dydnik@hotmail.com>
2023-08-03 20:51:40 +0300

    Add & Div have implemented; Remove deprecated test suit

--- src/main/java/org/unicalc/UniCalc.java
+++ src/main/java/org/unicalc/UniCalc.java
@@ -2,6 +2,23 @@ package org.unicalc;
 
 public class UniCalc {
 
-    public UniCalc() {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(float a, float b) {
+        return Math.round(a + b);
+    }
+
+    public int divInt(int a, int b) throws IllegalArgumentException {
+        if (b == 0)
+            throw new IllegalArgumentException();
+        return a / b;
+    }
+
+    public int divFloat(float a, float b) throws IllegalArgumentException {
+        if (b == 0)
+            throw new IllegalArgumentException();
+        return Math.round(a / b);
     }
 }
---
+++ src/test/java/org/unicalc/UniCalcMathTest.java
@@ -0,0 +1,99 @@
+package org.unicalc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class UniCalcMathTest {
+
+    @Test
+    public void testAddInt() {
+        //  Given
+
+        UniCalc calc = new UniCalc();
+
+        //  When
+
+        int result = calc.add(1, 2);
+
+        //  Then
+
+        int expected = 3;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAddFloat() {
+        //  Given
+
+        UniCalc calc = new UniCalc();
+
+        //  When
+
+        int result = calc.add(1f, 2f);
+
+        //  Then
+
+        int expected = 3;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDivInt() {
+        //  Given
+
+        UniCalc calc = new UniCalc();
+
+        //  When
+
+        int result = calc.divInt(1, 2);
+
+        //  Then
+
+        int expected = 1 / 2;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDivByZeroInt() {
+        //  Given
+
+        UniCalc calc = new UniCalc();
+
+        //  When & Then
+
+        int expected = 3;
+        assertThrows(IllegalArgumentException.class,
+                () -> calc.divInt(1, 0));
+    }
+
+    @Test
+    public void testDivFloat() {
+        //  Given
+
+        UniCalc calc = new UniCalc();
+
+        //  When
+
+        int result = calc.divFloat(1f, 2f);
+
+        //  Then
+
+        int expected = Math.round(1f / 2f);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDivByZeroFloat() {
+        //  Given
+
+        UniCalc calc = new UniCalc();
+
+        //  When & Then
+
+        int expected = 3;
+        assertThrows(IllegalArgumentException.class,
+                () -> calc.divFloat(1f, 0f));
+    }
+}
